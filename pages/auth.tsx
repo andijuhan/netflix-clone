688e1c0e956126f854205c3578ac97b868ebc12f
andijuhan <80870917+andijuhan@users.noreply.github.com>
2023-04-02 04:56:34 +0700

    add auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import axios from 'axios';
 import { useCallback, useState } from 'react';
 import Input from '../components/Input';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
-import type { NextPage } from 'next';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
+import Navbar from '../components/Navbar';
 
-const Home: NextPage = () => {
+export const getServerSideProps = async (context: NextPageContext) => {
+   const session = await getSession(context);
+
+   if (!session) {
+      return {
+         redirect: {
+            destination: '/auth',
+            permanent: false,
+         },
+      };
+   }
+
+   return {
+      props: {},
+   };
+};
+
+const Home = () => {
    return (
-      <>
-         <h1>Netflix Clone</h1>
-      </>
+      <div>
+         <Navbar />
+      </div>
    );
 };
 
